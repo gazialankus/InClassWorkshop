1448c4b2c2ac0c17f445d7b47988274e4a35996b
Gazihan Alankus <gazialankus@gmail.com>
2015-04-14 10:20:28 +0300

    Added MyFunc()

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,11 +5,15 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		MyFunc();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void MyFunc() {
+		Debug.Log("myfunc was called");
 	}
 }
